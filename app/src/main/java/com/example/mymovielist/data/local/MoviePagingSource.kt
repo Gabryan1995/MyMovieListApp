05273d160d81dbfcb91d644b22abcaa23def29b1
Garrett Bryan <gabryan1995@gmail.com>
2022-07-07 12:14:54 -0500

    Added PagingSource
    Added PagingSource to handle paging movies.

---
+++ app/src/main/java/com/example/mymovielist/data/local/MoviePagingSource.kt
@@ -0,0 +1,48 @@
+package com.example.mymovielist.data.local
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.mymovielist.data.dto.MovieResult
+import com.example.mymovielist.network.MovieApi
+import okio.IOException
+import retrofit2.HttpException
+
+@ExperimentalStdlibApi
+class MoviePagingSource(
+    private val apiKey: String
+) : PagingSource<Int, MovieResult>() {
+    override suspend fun load(
+        params: LoadParams<Int>
+    ): LoadResult<Int, MovieResult> {
+        return try {
+            // Start refresh at page 1 if undefined.
+            val nextPageNumber = params.key ?: 1
+            val response = MovieApi.retrofitService.getTopRated(apiKey, nextPageNumber)
+            LoadResult.Page(
+                data = response.results,
+                prevKey = null, // Only paging forward.
+                nextKey = response.nextPage
+            )
+        } catch (e: IOException) {
+            // IOException for network failures.
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            // HttpException for any non-2xx HTTP status codes.
+            return LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, MovieResult>): Int? {
+        // Try to find the page key of the closest page to anchorPosition, from
+        // either the prevKey or the nextKey, but you need to handle nullability
+        // here:
+        //  * prevKey == null -> anchorPage is the first page.
+        //  * nextKey == null -> anchorPage is the last page.
+        //  * both prevKey and nextKey null -> anchorPage is the initial page, so
+        //    just return null.
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+}
--- app/src/main/java/com/example/mymovielist/ui/browse/BrowseViewModel.kt
+++ app/src/main/java/com/example/mymovielist/ui/browse/BrowseViewModel.kt
@@ -5,12 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
+import androidx.paging.*
 import com.example.mymovielist.R
 import com.example.mymovielist.base.BaseViewModel
 import com.example.mymovielist.data.MovieDataSource
 import com.example.mymovielist.data.dto.*
 import com.example.mymovielist.data.dto.Result.*
+import com.example.mymovielist.data.local.MoviePagingSource
 import kotlinx.coroutines.launch
 import java.util.*
 
@@ -49,49 +50,60 @@ class BrowseViewModel(
 
     fun loadMovies() {
         showLoading.value = true
-        viewModelScope.launch {
-            //interacting with the dataSource has to be through a coroutine
-            val result = dataSource.getMovies(getApplication<Application>().applicationContext.getString(R.string.moviedb_key))
-            showLoading.postValue(false)
-            when (result) {
-                is Result.Success<*> -> {
-                    val dataList = ArrayList<MoviesPage>()
-                    dataList.addAll((result.data as List<MoviesPage>).map { movie ->
-                        //map the reminder data from the DB to the be ready to be displayed on the UI
-                        MoviesPage(
-                            movie.page,
-                            movie.results,
-                            movie.wResults,
-                            movie.totalResults,
-                            movie.totalPages,
-                            movie.id,
-                            movie.movieType
-                        )
-                    })
-                    for (movie in dataList) {
-                        when(movie.movieType) {
-                            MovieType.TOP_RATED -> _topRatedMovies.value = movie
-                            MovieType.POPULAR -> _popularMovies.value = movie
-                            MovieType.NOW_PLAYING -> _nowPlayingMovies.value = movie
-                        }
-                        _status.value = MoviesApiStatus.DONE
-                    }
-                }
-                is Result.Error -> {
-                    _topRatedMovies.value = MoviesPage(movieType = MovieType.TOP_RATED)
-                    _popularMovies.value = MoviesPage(movieType = MovieType.POPULAR)
-                    _nowPlayingMovies.value = MoviesPage(movieType = MovieType.NOW_PLAYING)
-                    _status.value = MoviesApiStatus.ERROR
-                    showSnackBar.value = result.message
-                }
-            }
-        }
-    }
+        _topRatedMovies.value = Pager(PagingConfig(pageSize = 20)) {
+            MoviePagingSource(getApplication<Application>().applicationContext.getString(R.string.moviedb_key))
+        }.liveData.cachedIn(viewModelScope).value
+
+        _popularMovies.value = Pager(PagingConfig(pageSize = 20)) {
+            MoviePagingSource(getApplication<Application>().applicationContext.getString(R.string.moviedb_key))
+        }.liveData.cachedIn(viewModelScope).value
+
+        _nowPlayingMovies.value = Pager(PagingConfig(pageSize = 20)) {
+            MoviePagingSource(getApplication<Application>().applicationContext.getString(R.string.moviedb_key))
+        }.liveData.cachedIn(viewModelScope).value
+
+//        viewModelScope.launch {
+//            //interacting with the dataSource has to be through a coroutine
+//            val result = dataSource.getMovies(getApplication<Application>().applicationContext.getString(R.string.moviedb_key))
+//            showLoading.postValue(false)
+//            when (result) {
+//                is Result.Success<*> -> {
+//                    val dataList = ArrayList<MoviesPage>()
+//                    dataList.addAll((result.data as List<MoviesPage>).map { movie ->
+//                        //map the reminder data from the DB to the be ready to be displayed on the UI
+//                        MoviesPage(
+//                            movie.page,
+//                            movie.results,
+//                            movie.wResults,
+//                            movie.totalResults,
+//                            movie.totalPages,
+//                            movie.id,
+//                            movie.movieType
+//                        )
+//                    })
+//                    for (movie in dataList) {
+//                        when(movie.movieType) {
+//                            MovieType.TOP_RATED -> _topRatedMovies.value = movie
+//                            MovieType.POPULAR -> _popularMovies.value = movie
+//                            MovieType.NOW_PLAYING -> _nowPlayingMovies.value = movie
+//                        }
+//                        _status.value = MoviesApiStatus.DONE
+//                    }
+//                }
+//                is Result.Error -> {
+//                    _topRatedMovies.value = MoviesPage(movieType = MovieType.TOP_RATED)
+//                    _popularMovies.value = MoviesPage(movieType = MovieType.POPULAR)
+//                    _nowPlayingMovies.value = MoviesPage(movieType = MovieType.NOW_PLAYING)
+//                    _status.value = MoviesApiStatus.ERROR
+//                    showSnackBar.value = result.message
+//                }
+//            }
+//        }
     }
 
     override fun onCleared() {
-        savedStateHandle[LAST_SEARCH_QUERY] = state.value.query
-        savedStateHandle[LAST_QUERY_SCROLLED] = state.value.lastQueryScrolled
+//        savedStateHandle[LAST_SEARCH_QUERY] = state.value.query
+//        savedStateHandle[LAST_QUERY_SCROLLED] = state.value.lastQueryScrolled
         super.onCleared()
     }
 
